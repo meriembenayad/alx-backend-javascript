2c6e39e7d0b22d525d4e6f02e6a21d75b5cb0666
meriembenayad <meriem.benayad@gmail.com>
2024-03-07 00:19:38 +0100

    Task 9: Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const value = mathFunction();
+    queue.push(value);
+  } catch(error) {
+    queue.push(`Error: ${error.message}`);
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
