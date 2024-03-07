5a99dec9dc67d5f7a63fba18552a5e72efaef5f3
meriembenayad <meriem.benayad@gmail.com>
2024-03-08 00:47:26 +0100

    Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
