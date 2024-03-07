b63281fc9827bfc6424ed0b97607f3c436dbdc66
meriembenayad <meriem.benayad@gmail.com>
2024-03-07 23:47:46 +0100

    Task 6: Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,26 +3,15 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._floors = floors;
+    this.floors = floors;
   }
 
   get sqft() {
     return this._floors;
   }
 
-  set sqft(newSqft) {
-    this._floors = newSqft;
-  }
-
   get floors() {
     return this._floors;
-  }
-
-  set floors(newFloors) {
-    if (typeof newFloors !== 'number') {
-      throw new TypeError('floors must be a number');
-    }
-    this._floors = newFloors;
   }
 
   evacuationWarningMessage() {
