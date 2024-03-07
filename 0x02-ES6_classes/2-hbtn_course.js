932599f2c99e84fc03739e872839673e6e45c768
meriembenayad <meriem.benayad@gmail.com>
2024-03-07 20:35:44 +0100

    Task 2: A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,5 +1,14 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    }
+    if (!(Array.isArray(students)) || !(students.every((student) => typeof student === 'string'))) {
+      throw new TypeError('student must be an array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -32,11 +41,8 @@ export default class HolbertonCourse {
   }
 
   set students(newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new TypeError('students must be an array');
-    }
-    if (!newStudents.every((student) => typeof student !== 'string')) {
-      throw new TypeError('student must be a strings');
+    if (!(Array.isArray(newStudents)) || !(newStudents.every((newStudent) => typeof newStudent === 'string'))) {
+      throw new TypeError('student must be an array of strings');
     }
     this._students = newStudents;
   }
