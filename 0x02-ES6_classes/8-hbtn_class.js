99922ea9888a9070d6784dd0ac415e13764983c1
meriembenayad <meriem.benayad@gmail.com>
2024-03-08 00:14:08 +0100

    Task 8: Primitive Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,20 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+    this._size = size;
+    this._location = location;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
