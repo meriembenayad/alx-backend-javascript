b34910e0d2096dd880eb61921a11cc90364614f4
meriembenayad <meriem.benayad@gmail.com>
2024-03-08 01:08:03 +0100

    Task 11: EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  cloneCar() {
+    return new Car();
+  }
+}
