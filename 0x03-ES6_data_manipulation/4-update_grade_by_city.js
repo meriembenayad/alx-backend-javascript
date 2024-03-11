647e2204ae537893452fcbd6d5432b0f6e7d8cb7
meriembenayad <meriem.benayad@gmail.com>
2024-03-11 16:43:17 +0000

    Task 4: Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
