8eb723defc054f2ca8bd571c656a3a4d7a797de2
meriembenayad <meriem.benayad@gmail.com>
2024-03-11 19:04:29 +0000

    Task 11: WeakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+const weakMap = new WeakMap();
+
+const queryAPI = (endpoint) => {
+  let count = weakMap.get(endpoint) || 0;
+  weakMap.set(endpoint, count += 1);
+  if (count >= 5) throw new Error('Endpoint load is high');
+  return count;
+};
+
+export { weakMap, queryAPI };
