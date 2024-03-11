f4c6a7f6f615c033dc0e44bcff2ebc919e9424fd
meriembenayad <meriem.benayad@gmail.com>
2024-03-11 17:43:40 +0000

    Task 8: Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function cleanSet(set, startString) {
+  if (startString === '') return '';
+  let result = '';
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      result += `${item.slice(startString.length)}-`;
+    }
+  }
+  return result.slice(0, -1);
+}
