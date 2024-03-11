64afe6ecd03b1e1ddc570b7998757878c2b2178c
meriembenayad <meriem.benayad@gmail.com>
2024-03-11 18:02:32 +0000

    Task 10: More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Can\'t process');
+  for (const el of map) {
+    if (el[1] === 1) {
+      map.set(el[0], 100);
+    }
+  }
+  return map;
+}
