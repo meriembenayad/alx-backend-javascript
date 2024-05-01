1ec614d1b274a1851a9ea3bade0b129c2d6123e7
meriembenayad <meriem.benayad@gmail.com>
2024-05-01 16:03:00 +0100

    Task 2: Reading a file synchronously with NodeJS

--- 0x05-Node_JS_basic/.eslintrc.js
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -1,27 +1,37 @@
 module.exports = {
-    "env": {
-        "node": true,
-	"browser": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
     }
+  ]
 };
--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -8,18 +8,18 @@ function countStudents(path) {
     // Attempt to read file synchronously
     data = fs.readFileSync(path, 'utf-8');
   } catch (err) {
-    throw new Error('Cannot load the database')
+    throw new Error('Cannot load the database');
   }
 
   // Split the file into lines
   const lines = data.split('\n');
 
   // filter out empty lines
-  const students = lines.filter(line => line).map(line => line.split(','));
+  const students = lines.filter((line) => line).map((line) => line.split(','));
 
   const fields = {};
 
-  for (let i = 1; i < students.length; i++) {
+  for (let i = 1; i < students.length; i += 1) {
     const field = students[i][3];
     const firstName = students[i][0];
     if (!fields[field]) {
@@ -30,7 +30,9 @@ function countStudents(path) {
 
   console.log(`Number of students: ${students.length - 1}`);
   for (const field in fields) {
-    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    if (Object.prototype.hasOwnProperty.call(fields, field)) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
   }
 }
 
