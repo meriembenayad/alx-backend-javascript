0e30995be870b7810ef2465c4029995120dacf2b
meriembenayad <meriem.benayad@gmail.com>
2024-05-01 19:21:18 +0100

    Task 7: Create more complex HTTP server using Express Module

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,24 @@
-#!/usr/bin/node
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  let students = '';
+  const consoleLog = console.log;
+  console.log = (msg) => { students += `\n${msg}`; };
+  await countStudents(process.argv[2]);
+  console.log = consoleLog;
+  res.send(`This is the list of our students${students}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
