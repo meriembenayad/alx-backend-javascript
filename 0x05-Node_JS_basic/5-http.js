3f3becc40af213e63c539a90535a01533b7e3148
meriembenayad <meriem.benayad@gmail.com>
2024-05-01 16:54:55 +0100

    Task 5: Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,1 +1,29 @@
-#!/usr/bin/node
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    try {
+      const students = await countStudents(process.argv[2]);
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`This is the list of our students\n${students}`);
+    } catch (error) {
+      res.statusCode = 500;
+      res.end(error.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Not found\n');
+  }
+});
+
+app.listen(1245, () => {
+  console.log('Server running at http://localhost:1245/');
+});
+
+module.exports = app;
