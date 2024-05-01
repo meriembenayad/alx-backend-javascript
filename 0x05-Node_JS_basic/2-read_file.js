dce39e693701d9c121a0fb0829f7d24744bf5f71
meriembenayad <meriem.benayad@gmail.com>
2024-05-01 15:44:16 +0100

    Task 2: Reading a file synchronously with NodeJS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,43 +3,35 @@ const fs = require('fs');
 
 // Define the countStudents function
 function countStudents(path) {
+  let data;
   try {
     // Attempt to read file synchronously
-    const data = fs.readFileSync(path, 'utf-8');
-
-    // Split the file into lines
-    const lines = data.split('\n');
-
-    // filter out empty lines
-    const emptyLine = lines.filter(line => line);
-
-    // count number of students
-    const numberOfStudents = emptyLine.length;
-    console.log(`Number of students: ${numberOfStudents}`);
-
-    // Dictionnary to store each field and corresponding students
-    const fieldDict = {};
-
-    for (let i = 1; i < emptyLine.length; i++) {
-      const student = emptyLine[i].split(',');
-      const field = student[3]
-      const firstName = student[0];
-
-      if (!fieldDict[field]) {
-        fieldDict[field] = [];
-      }
-
-      fieldDict[field].push(firstName);
-    }
-
-    // Log the number of students on each field and the list of the fist names
-    for (const field in fieldDict) {
-      console.log(`Number of students in ${field}: ${fieldDict[field].length}. List: ${fieldDict[field].join(', ')}`);
-    }
+    data = fs.readFileSync(path, 'utf-8');
   } catch (err) {
-    throw new Error('Cannot load the database');
+    throw new Error('Cannot load the database')
   }
 
+  // Split the file into lines
+  const lines = data.split('\n');
+
+  // filter out empty lines
+  const students = lines.filter(line => line).map(line => line.split(','));
+
+  const fields = {};
+
+  for (let i = 1; i < students.length; i++) {
+    const field = students[i][3];
+    const firstName = students[i][0];
+    if (!fields[field]) {
+      fields[field] = [];
+    }
+    fields[field].push(firstName);
+  }
+
+  console.log(`Number of students: ${students.length - 1}`);
+  for (const field in fields) {
+    console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+  }
 }
 
 // Export the function
