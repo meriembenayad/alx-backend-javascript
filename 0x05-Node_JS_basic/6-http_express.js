ecded4a23053ff4019a930a43d435e34489b642d
meriembenayad <meriem.benayad@gmail.com>
2024-05-01 19:09:31 +0100

    Task 6: Create a small HTTP server using Express Module

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {});
+
+module.exports = app;
