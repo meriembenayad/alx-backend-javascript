291c38842294bfb7405222c255d7ab1fee9ebab6
meriembenayad <meriem.benayad@gmail.com>
2024-05-05 14:45:36 +0100

    Task 5: Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -4,24 +4,24 @@ const countStudents = require('./3-read_file_async');
 const app = http.createServer(async (req, res) => {
   if (req.url === '/') {
     res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
+    res.setHeader('Content-Type', 'text/plain');
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    if (!process.argv[2]) {
+    try {
+      let students = '';
+      const consoleLog = console.log;
+      console.log = (msg) => { students += `\n${msg}`; };
+      await countStudents(process.argv[2]);
+      console.log = consoleLog;
       res.statusCode = 200;
-      res.setHeader('Content-type', 'text/plain');
-      res.end('This list of our students\nDatabase not available');
-    } else {
-      try {
-        let students = '';
-        const consoleLog = console.log;
-        console.log = (msg) => { students += `\n${msg}`; };
-        await countStudents(process.argv[2]);
-        console.log = consoleLog;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end(`This is the list of our students${students}`);
+    } catch (error) {
+      if (error.message === 'Cannot load the database') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(`This is the list of our students${students}`);
-      } catch (error) {
+        res.end('This is the list of our students\nDatabase not available');
+      } else {
         res.statusCode = 500;
         res.end(error.message);
       }
